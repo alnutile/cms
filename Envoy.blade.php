c28e71322601da551e7a4807cb8f94f9f7ad43cf
alnutile <alfrednutile@gmail.com>
2014-05-15 07:47:22 -0400

    Perms

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -4,6 +4,7 @@
   cd /var/www/cms/site/current
   chmod 777 public/img
   chmod 777 public/img/banners
+  chmod -R 775 app/storage
   git pull origin development
   composer install 
   php artisan migrate:refresh --seed
