6a274b705763f5b7b494b15840c44bd6bd1b78b4
Alfred Nutile <alfrednutile@gmail.com>
2014-07-27 19:58:02 -0400

    Update database.php

--- app/config/development/database.php
+++ app/config/development/database.php
@@ -48,7 +48,7 @@ return array(
 
 		'sqlite' => array(
 			'driver'   => 'sqlite',
-			'database' => __DIR__.'/../../database/local.sqlite',
+			'database' => __DIR__.'/../../database/development.sqlite',
 			'prefix'   => '',
 		),
 
