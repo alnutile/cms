d9340626f8ee773810d63e17956d139674904ae4
Andy Cavanagh <andy@zigbop>
2014-07-27 20:03:03 -0500

    Fixes #29 works and tested

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -4,110 +4,127 @@ use Symfony\Component\Filesystem\Filesystem;
 
 class BaseController extends Controller {
 
-    protected $filesystem;
-    public $settings;
-    public $portfolio;
-    protected $banner = FALSE;
+  protected $filesystem;
+  public $settings;
+  public $portfolio;
+  protected $banner = FALSE;
 
-    public function __construct(Setting $settings = null, Portfolio $portfolio = null, Filesystem $filesystem = null)
+  public function __construct(Setting $settings = null, Portfolio $portfolio = null, Filesystem $filesystem = null)
+  {
+    $this->settings = ($settings == null) ? Setting::first() : $settings;
+    $this->portfolio = ($portfolio == null) ? Portfolio::all() : $portfolio;
+    $this->filesystem = ($filesystem == null) ? new Filesystem : $filesystem;
+    \View::share('settings', $this->settings);
+  }
+  /**
+   * Setup the layout used by the controller.
+   *
+   * @return void
+   */
+  protected function setupLayout()
+  {
+    if ( ! is_null($this->layout))
     {
-        $this->settings = ($settings == null) ? Setting::first() : $settings;
-        $this->portfolio = ($portfolio == null) ? Portfolio::all() : $portfolio;
-        $this->filesystem = ($filesystem == null) ? new Filesystem : $filesystem;
-        \View::share('settings', $this->settings);
+      $this->layout = View::make($this->layout);
     }
-	/**
-	 * Setup the layout used by the controller.
-	 *
-	 * @return void
-	 */
-	protected function setupLayout()
-	{
-		if ( ! is_null($this->layout))
-		{
-			$this->layout = View::make($this->layout);
-		}
-	}
+  }
 
-    public function json_response($status, $message, $data, $code) {
-        return Response::json(['status' => $status, 'message' => $message, 'data' => $data], $code);
-    }
+  public function json_response($status, $message, $data, $code) {
+    return Response::json(['status' => $status, 'message' => $message, 'data' => $data], $code);
+  }
 
-    public function respond($results, $view, $view_options, $message = null)
-    {
-        if(Request::format() == 'html') {
-            if(!$results) {
-                return View::make('404');
-            }
-            return View::make($view, $view_options);
-        } else {
-            if(!$results) {
-                return Response::json(null, 404);
-            }
-            return Response::json(array('data' => $results->toArray(), 'status'=>'success', 'message' => "Success"), 200);
-        }
-    }
-
-    public function bannerSet($page)
-    {
-        if (isset($page) && $page->slug === '/home') {
-            $banner = TRUE;
-        } else {
-            $banner = FALSE;
-        }
-        return $banner;
-    }
-
-    public function getPortfolioBlock()
-    {
-        return Portfolio::allActiveSorted();
-    }
-
-    public function uploadFile($data, $field_name)
-    {
-        //Only run when an image
-        if($data[$field_name])
-        {
-
-            $image = $data[$field_name];
-            $filename = $image->getClientOriginalName();
-            $destination = $this->save_to;
-
-            if(!$this->filesystem->exists($destination)) {
-                $this->filesystem->mkdir($destination);
-            }
-            try {
-                $image->move($destination, $filename);
-                $data[$field_name] = $filename;
-            } catch(\Exception $e) {
-                throw new \Exception("Error uploading file $field_name" . $e->getMessage());
-            }
-        }
-        return $data;
-    }
-
-    public function validateSlug($all, $model, $rules)
-    {
-      $messages = [];
-      if(isset($all['slug']) && $all['slug'] != $model->slug) {
-        $messages = array(
-          'slug.unique' => 'The url is not unique to the Portfolios table.',
-          'slug.regex' => 'The url must start with a slash and contain only letters and numbers, no spaces.'
-        );
-      } else {
-        unset($rules['slug']);
+  public function respond($results, $view, $view_options, $message = null)
+  {
+    if(Request::format() == 'html') {
+      if(!$results) {
+        return View::make('404');
       }
-      $validator = Validator::make($data = Input::all(), $rules, $messages);
-      return $validator;
+      return View::make($view, $view_options);
+    } else {
+      if(!$results) {
+        return Response::json(null, 404);
+      }
+      return Response::json(array('data' => $results->toArray(), 'status'=>'success', 'message' => "Success"), 200);
     }
+  }
 
-    public function checkPublished($data)
+  public function bannerSet($page)
+  {
+    if (isset($page) && $page->slug === '/home') {
+      $banner = TRUE;
+    } else {
+      $banner = FALSE;
+    }
+    return $banner;
+  }
+
+  public function getPortfolioBlock()
+  {
+    return Portfolio::allActiveSorted();
+  }
+
+  public function uploadFile($data, $field_name)
+  {
+    //Only run when an image
+    if($data[$field_name])
     {
-        if(!isset($data['published'])) {
-            $data['published'] = 0;
-        }
 
-        return $data;
+      $image = $data[$field_name];
+      $filename = $image->getClientOriginalName();
+      $destination = $this->save_to;
+
+      if(!$this->filesystem->exists($destination)) {
+        $this->filesystem->mkdir($destination);
+      }
+      try {
+        $image->move($destination, $filename);
+        $data[$field_name] = $filename;
+      } catch(\Exception $e) {
+        throw new \Exception("Error uploading file $field_name" . $e->getMessage());
+      }
     }
+    return $data;
+  }
+
+
+  /**
+   * @param $all
+   * @param $model
+   * @param $rules Portfolios::$rules
+   * @return \Illuminate\Validation\Validator
+   */
+  public function validateSlugEdit($all, $model, $rules)
+  {
+    $messages = [];
+    if(isset($all['slug']) && $all['slug'] != $model->slug) {
+      $messages = array(
+        'slug.unique' => 'The url is not unique.',
+        'slug.regex' => 'The url must start with a slash and contain only letters and numbers, no spaces.'
+      );
+    } else {
+      unset($rules['slug']);
+    }
+    $validator = Validator::make($data = Input::all(), $rules, $messages);
+    return $validator;
+  }
+
+  public function validateSlugOnCreate($all, $rules)
+  {
+    $messages = array(
+      'slug.unique' => 'The url is not unique .',
+      'slug.regex' => 'The url must start with a slash and contain only letters and numbers, no spaces.'
+    );
+    $validator = Validator::make($data = Input::all(), $rules, $messages);
+    return $validator;
+  }
+
+  public function checkPublished($data)
+  {
+    if(!isset($data['published'])) {
+      $data['published'] = 0;
+    }
+
+    return $data;
+  }
 
 }
--- app/controllers/PortfoliosController.php
+++ app/controllers/PortfoliosController.php
@@ -2,114 +2,104 @@
 
 class PortfoliosController extends \BaseController {
 
-    public $portfolio;
+  public $portfolio;
 
-    public function __construct(Portfolio $portfolio = null)
-    {
-        parent::__construct();
-        $this->beforeFilter("auth", array('only' => ['adminIndex', 'create', 'delete', 'edit', 'update', 'store']));
-        $this->portfolio = ($portfolio == null) ? new Portfolio : $portfolio;
+  public function __construct(Portfolio $portfolio = null)
+  {
+    parent::__construct();
+    $this->beforeFilter("auth", array('only' => ['adminIndex', 'create', 'delete', 'edit', 'update', 'store']));
+    $this->portfolio = ($portfolio == null) ? new Portfolio : $portfolio;
+  }
+
+
+  /**
+   * Display a listing of portfolios
+   *
+   * @return Response
+   */
+  public function index()
+  {
+    $portfolios = Portfolio::Published()->OrderByOrder()->get();
+
+    return View::make('portfolios.index', compact('portfolios'));
+  }
+
+  /**
+   * Display a listing of portfolios
+   *
+   * @return Response
+   */
+  public function adminIndex()
+  {
+    $portfolios = Portfolio::OrderByOrder()->get();
+
+    return View::make('portfolios.admin_index', compact('portfolios'));
+  }
+
+  /**
+   * Show the form for creating a new portfolio
+   *
+   * @return Response
+   */
+  public function create()
+  {
+    return View::make('portfolios.create');
+  }
+
+  /**
+   * Store a newly created portfolio in storage.
+   *
+   * @return Response
+   */
+  public function store()
+  {
+    $all = Input::all();
+    $rules = Portfolio::$rules;
+    $this->validateSlugOnCreate($all, $rules);
+
+    Portfolio::create($all);
+
+    return Redirect::route('admin_portfolio');
+  }
+
+  /**
+   * Display the specified portfolio.
+   *
+   * @param  int  $id
+   * @return Response
+   */
+  public function show($portfolio)
+  {
+    if(is_numeric($portfolio)) {
+      $portfolio = Portfolio::find($portfolio);
     }
 
+    $seo = $portfolio->seo;
+    $banner = FALSE;
+    return View::make('portfolios.show', compact('portfolio', 'banner', 'settings', 'seo'));
+  }
 
-	/**
-	 * Display a listing of portfolios
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
-		$portfolios = Portfolio::Published()->OrderByOrder()->get();
+  /**
+   * Show the form for editing the specified portfolio.
+   *
+   * @param  int  $id
+   * @return Response
+   */
+  public function edit($id)
+  {
+    $portfolio = Portfolio::find($id);
 
-		return View::make('portfolios.index', compact('portfolios'));
-	}
+    return View::make('portfolios.edit', compact('portfolio'));
+  }
 
-    /**
-     * Display a listing of portfolios
-     *
-     * @return Response
-     */
-    public function adminIndex()
-    {
-        $portfolios = Portfolio::OrderByOrder()->get();
-
-        return View::make('portfolios.admin_index', compact('portfolios'));
-    }
-
-	/**
-	 * Show the form for creating a new portfolio
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-		return View::make('portfolios.create');
-	}
-
-	/**
-	 * Store a newly created portfolio in storage.
-	 *
-	 * @return Response
-	 */
-	public function store()
-	{
-    $messages = array(
-      'slug.unique' => 'The url is not unique to the Portfolios table.',
-      'slug.regex' => 'The url must start with a slash and contain only letters and numbers, no spaces.'
-    );
-
-    $validator = Validator::make($data = Input::all(), Portfolio::$rules, $messages);
-
-		if ($validator->fails())
-		{
-      $messages = $validator->messages();
-			return Redirect::back()->withErrors($validator)->withInput();
-
-		}
-
-		Portfolio::create($data);
-
-		return Redirect::route('admin_portfolio');
-	}
-
-	/**
-	 * Display the specified portfolio.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-    public function show($portfolio)
-    {
-        if(is_numeric($portfolio)) {
-            $portfolio = Portfolio::find($portfolio);
-        }
-
-        $seo = $portfolio->seo;
-        $banner = FALSE;
-        return View::make('portfolios.show', compact('portfolio', 'banner', 'settings', 'seo'));
-    }
-
-	/**
-	 * Show the form for editing the specified portfolio.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-		$portfolio = Portfolio::find($id);
-
-		return View::make('portfolios.edit', compact('portfolio'));
-	}
-
-	/**
-	 * Update the specified portfolio in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
-	{
+  /**
+   * Update the specified portfolio in storage.
+   *
+   * @param  int  $id
+   * @return Response
+   */
+  public function update($id)
+  {
     $portfolio = Portfolio::findOrFail($id);
     $messages = [];
 
@@ -118,29 +108,29 @@ class PortfoliosController extends \BaseController {
     $all = Input::all();
     $rules = Portfolio::$rules;
 
-    $validator = $this->validateSlug($all, $portfolio, $rules);
+    $validator = $this->validateSlugEdit($all, $portfolio, $rules);
     $data = $this->checkPublished($all);
     if ($validator->fails())
-		{
-			return Redirect::back()->withErrors($validator)->withInput();
-		}
+    {
+      return Redirect::back()->withErrors($validator)->withInput();
+    }
 
-		$portfolio->update($data);
+    $portfolio->update($data);
 
-		return Redirect::route('admin_portfolio');
-	}
+    return Redirect::route('admin_portfolio');
+  }
 
-	/**
-	 * Remove the specified portfolio from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		Portfolio::destroy($id);
+  /**
+   * Remove the specified portfolio from storage.
+   *
+   * @param  int  $id
+   * @return Response
+   */
+  public function destroy($id)
+  {
+    Portfolio::destroy($id);
 
-		return Redirect::route('portfolios.index');
-	}
+    return Redirect::route('portfolios.index');
+  }
 
 }
