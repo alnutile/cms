4472808317a939383e223ea8754298187e83e341
Andrew Cavanagh <cavanaghacea@gmail.com>
2014-08-21 20:54:28 -0400

    fixes #42 no way to destroy projects

--- app/views/projects/edit.blade.php
+++ app/views/projects/edit.blade.php
@@ -3,77 +3,77 @@
 @section('content')
 
 <div class="col-md-3 ">
-    @include('shared.sidebar')
+  @include('shared.sidebar')
 </div>
 
 <div class="col-md-9 column">
 
-    <h2>Update Project: {{$project->title}} </h2>
+  <h2>Update Project: {{$project->title}} </h2>
 
-    {{ Form::model($project, array('method' => 'PUT', 'route' => array('projects.update', $project->id), 'files' => 'true', 'role' => 'form')) }}
+  {{ Form::model($project, array('method' => 'PUT', 'route' => array('projects.update', $project->id), 'files' => 'true', 'role' => 'form')) }}
 
 
-    <div class="form-group">
-        <label>Title</label>
-        {{ Form::text('title', null, array('class' => 'form-control')) }}
+  <div class="form-group">
+    <label>Title</label>
+    {{ Form::text('title', null, array('class' => 'form-control')) }}
+  </div>
+  @if($errors->first('title'))
+  <div class="alert alert-danger">
+    {{  $errors->first('title'); }}
+  </div>
+  @endif
+
+  <div class="form-group">
+    <label>Seo Title</label>
+    {{ Form::text('seo', null, array('class' => 'form-control')) }}
+  </div>
+  @if($errors->first('seo'))
+  <div class="alert alert-danger">
+    {{  $errors->first('seo'); }}
+  </div>
+  @endif
+
+
+  <div class="form-group">
+    <label>Body</label>
+    {{ Form::textarea('body', null, array('rows' => 30, 'class' => 'ckeditor form-control')) }}
+  </div>
+  @if($errors->first('body'))
+  <div class="alert alert-danger">
+    {{  $errors->first('body'); }}
+  </div>
+  @endif
+
+
+  <div class="form-group">
+    <div class="controls">
+      <div class="checkbox">
+        <label class="checkbox">{{ Form::checkbox('published', 1) }} Published</label>
+      </div>
     </div>
-    @if($errors->first('title'))
+  </div>
+
+  <div class="form-group">
+    <label for="email">Related Portfolios</label>&nbsp;
+    {{ Form::select('portfolio_id', $portfolios, $project->portfolio_id, array('class' => 'form-control', 'tabindex' => 1)) }}
+    @if($errors->first('order'))
     <div class="alert alert-danger">
-        {{  $errors->first('title'); }}
+      {{  $errors->first('portfolio_id'); }}
     </div>
     @endif
+  </div>
 
-    <div class="form-group">
-        <label>Seo Title</label>
-        {{ Form::text('seo', null, array('class' => 'form-control')) }}
-    </div>
-    @if($errors->first('seo'))
+  <!--sort order-->
+
+  <div class="form-group">
+    <label for="email">Sort Order</label>&nbsp;
+    {{ Form::selectRange('order', 1, 10, array('class' => 'form-control', 'tabindex' => 1)) }}
+    @if($errors->first('order'))
     <div class="alert alert-danger">
-        {{  $errors->first('seo'); }}
+      {{  $errors->first('order'); }}
     </div>
     @endif
-
-
-    <div class="form-group">
-        <label>Body</label>
-        {{ Form::textarea('body', null, array('rows' => 30, 'class' => 'ckeditor form-control')) }}
-    </div>
-    @if($errors->first('body'))
-    <div class="alert alert-danger">
-        {{  $errors->first('body'); }}
-    </div>
-    @endif
-
-
-    <div class="form-group">
-        <div class="controls">
-            <div class="checkbox">
-                <label class="checkbox">{{ Form::checkbox('published', 1) }} Published</label>
-            </div>
-        </div>
-    </div>
-
-    <div class="form-group">
-        <label for="email">Related Portfolios</label>&nbsp;
-        {{ Form::select('portfolio_id', $portfolios, $project->portfolio_id, array('class' => 'form-control', 'tabindex' => 1)) }}
-        @if($errors->first('order'))
-        <div class="alert alert-danger">
-            {{  $errors->first('portfolio_id'); }}
-        </div>
-        @endif
-    </div>
-
-    <!--sort order-->
-
-    <div class="form-group">
-        <label for="email">Sort Order</label>&nbsp;
-        {{ Form::selectRange('order', 1, 10, array('class' => 'form-control', 'tabindex' => 1)) }}
-        @if($errors->first('order'))
-        <div class="alert alert-danger">
-            {{  $errors->first('order'); }}
-        </div>
-        @endif
-    </div>
+  </div>
 
 
   <div class="form-group">
@@ -88,43 +88,45 @@
     @endif
   </div>
   @endif
-    <!-- image -->
+  <!-- image -->
 
-    <div class="form-group">
-        <label for="email">Upload Main Image</label>
-        {{ Form::file('image', null, array('class' => 'form-control', 'tabindex' => 1)) }}
-        @if($errors->first('image'))
-        <div class="alert alert-danger">
-            {{  $errors->first('image'); }}
-        </div>
-        @endif
-        @if($project->image)
-            <div class="row">
-                <div>
-                    <img  class="col-lg-4" src="/{{$path}}/{{$project->image}}" class="banner-show">
-                </div>
-            </div>
-        @endif
-        <div class="help-block">This is the image we will use for the default project image</div>
+  <div class="form-group">
+    <label for="email">Upload Main Image</label>
+    {{ Form::file('image', null, array('class' => 'form-control', 'tabindex' => 1)) }}
+    @if($errors->first('image'))
+    <div class="alert alert-danger">
+      {{  $errors->first('image'); }}
     </div>
-
-    <br>
-    <br>
-
-    <!-- images upload -->
-
-    @include('projects.images_angular')
-
-    <br>
-    <br>
-    <!-- end images upload -->
-
-    <div class="controls">
-        {{ Form::submit('Update Project', array('id' => 'submit', 'class' => 'btn btn-success')) }}
-        <br>
+    @endif
+    @if($project->image)
+    <div class="row">
+      <div>
+        <img  class="col-lg-4" src="/{{$path}}/{{$project->image}}" class="banner-show">
+      </div>
     </div>
+    @endif
+    <div class="help-block">This is the image we will use for the default project image</div>
+  </div>
 
-    {{ Form::close() }}
+  <br>
+  <br>
+
+  <!-- images upload -->
+
+  @include('projects.images_angular')
+
+  <br>
+  <br>
+  <!-- end images upload -->
+
+  <div class="controls">
+    {{ Form::submit('Update Project', array('id' => 'submit', 'class' => 'btn btn-success')) }}
+    {{ Form::open(array('class' => 'inline', 'method' => 'DELETE', 'route' => array('projects.destroy', $project->id))) }}
+    {{ Form::submit('Delete', array('class' => 'btn btn-danger', 'onclick' => 'return confirm("Are you sure you want to delete this?")')) }}
+    <br>
+  </div>
+
+  {{ Form::close() }}
 
 
 </div>
