d014e7c324a0580d63c4154794fcf93b5c3706ed
John M. Corbett <meetinghouse@users.noreply.github.com>
2014-09-22 16:22:17 -0400

    Update edit.blade.php

--- app/views/settings/edit.blade.php
+++ app/views/settings/edit.blade.php
@@ -14,7 +14,7 @@
     <input type="hidden" name="_token" value="{{{ Session::getToken() }}}">
     <fieldset>
         <div class="form-group">
-            <label for="email">Company Name</label>
+            <label for="email">Company Name (<a href="http://www.restorationtrades.com/help.html#company_name">Help</a>)</label>
             {{ Form::text('name', null, array('class' => 'form-control', 'tabindex' => 1)) }}
             @if($errors->first('name'))
             <div class="alert alert-danger">
@@ -24,7 +24,7 @@
         </div>
 
         <div class="form-group">
-                <label for="logo">Upload Logo</label>
+                <label for="logo">Upload Logo (<a href="http://www.restorationtrades.com/help.html#company_logo_image_uploader">Help</a>)</label>
                 {{ Form::file('logo', null, array('class' => 'form-control', 'tabindex' => 2)) }}
                 @if($errors->first('logo'))
                 <div class="alert alert-danger">
@@ -39,12 +39,12 @@
                     <div class="checkbox">
                         <label class="checkbox">{{ Form::checkbox('remove_logo', null) }} Remove Logo</label>
                     </div>
-                    <div class="help-block">If you want no logo</div>
+                    <div class="help-block">If you want no logo (Company Name will show).</div>
                 </div>
             </div>
         @endif
         <div class="form-group">
-                <label for="email">Colors</label>&nbsp;
+                <label for="email">Colors (<a href="http://www.restorationtrades.com/help.html#colors">Help</a>)</label>&nbsp;
                 {{ Form::select('color', ['bw' => 'black & white', 'orange' => 'orange', 'green' => 'green', 'blue' => 'blue'], $setting->color, array('class' => 'form-control', 'tabindex' => 4)) }}
                 @if($errors->first('color'))
                 <div class="alert alert-danger">
@@ -54,7 +54,7 @@
         </div>
         
         <div class="form-group">
-                <label for="email">Facebook</label>
+                <label for="email">Facebook (<a href="http://www.restorationtrades.com/help.html#social_media_links">Help</a>)</label>
                 {{ Form::text('facebook', null, array('class' => 'form-control', 'tabindex' => 6)) }}
                 @if($errors->first('facebook'))
                 <div class="alert alert-danger">
@@ -64,7 +64,7 @@
         </div>
 
         <div class="form-group">
-                <label for="email">Linkedin</label>
+                <label for="email">Linkedin (<a href="http://www.restorationtrades.com/help.html#social_media_links">Help</a>)</label>
                 {{ Form::text('linkedin', null, array('class' => 'form-control', 'tabindex' => 8)) }}
                 @if($errors->first('linkedin'))
                 <div class="alert alert-danger">
@@ -74,7 +74,7 @@
         </div>
 
         <div class="form-group">
-                <label for="email">Twitter</label>
+                <label for="email">Twitter (<a href="http://www.restorationtrades.com/help.html#social_media_links">Help</a>)</label>
                 {{ Form::text('twitter', null, array('class' => 'form-control', 'tabindex' => 10)) }}
                 @if($errors->first('twitter'))
                 <div class="alert alert-danger">
@@ -84,7 +84,7 @@
         </div>
 
         <div class="form-group">
-                <label for="email">Pinterest</label>
+                <label for="email">Pinterest (<a href="http://www.restorationtrades.com/help.html#social_media_links">Help</a>)</label>
                 {{ Form::text('pinterest', null, array('class' => 'form-control', 'tabindex' => 12)) }}
                 @if($errors->first('pinterest'))
                 <div class="alert alert-danger">
@@ -93,7 +93,7 @@
                 @endif
         </div>
         <div class="form-group">
-            <label for="email">Google Plus</label>
+            <label for="email">Google Plus (<a href="http://www.restorationtrades.com/help.html#social_media_links">Help</a>)</label>
             {{ Form::text('gplus', null, array('class' => 'form-control', 'tabindex' => 14)) }}
             @if($errors->first('gplus'))
             <div class="alert alert-danger">
@@ -102,9 +102,9 @@
             @endif
         </div>
         <div class="form-group">
-            <label>Footer</label>
+            <label>Page Footer (<a href="http://www.restorationtrades.com/help.html#page_footer">Help</a>)</label>
             {{ Form::textarea('footer', $setting->footer, array('rows' => 30, 'class' => 'ckeditor form-control')) }}
-            <div class="help-block">Add your contact info and other info needed.</div>
+            <div class="help-block">Add your contact, copyright and other info needed.</div>
         </div>
         @if($errors->first('body'))
             <div class="alert alert-danger">
@@ -115,9 +115,9 @@
         <div class="form-group">
             <div class="controls">
                 <div class="checkbox">
-                    <label class="checkbox">{{ Form::checkbox('maintenance_mode', null) }} Maintenance Mode</label>
+                    <label class="checkbox">{{ Form::checkbox('maintenance_mode', null) }} Maintenance Mode (<a href="http://www.restorationtrades.com/help.html#maintenance_mode">Help</a>)</label>
                 </div>
-                <div class="help-block">If you want the robot.txt hidden from search engines</div>
+                <div class="help-block">If you want the website hidden from the Visitor AND the search engines, until you are ready to publish (<a href="http://www.restorationtrades.com/help.html#maintenance_mode">Help</a>).</div>
             </div>
         </div>
 
