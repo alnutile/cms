631c9895bca8d9c254e8a71bccb2ceb30ce9a45d
alnutile <alfrednutile@gmail.com>
2014-10-05 18:52:10 -0400

    fixes issue #10

--- app/views/shared/projects_teasers.blade.php
+++ app/views/shared/projects_teasers.blade.php
@@ -1,7 +1,11 @@
+
 <?php if(empty($project->image)) { $image = 'project3.jpg'; } else { $image = $project->image; } ?>
+
 <div class="media row">
   <img class="media-object col-lg-4" src="/img/projects/{{$image}}" alt="{{{$project->title}}}">
+
   <br>
+
   <div class="media-body">
     <a href="{{$project->slug}}"><h4 class="media-heading">{{{$project->title}}}</h4></a>
     <div class="row">
@@ -11,7 +15,11 @@
         {{{$project->state_country}}}
       </div>
     </div>
-    <p>{{Str::words($project->body, 50)}}</p>
+    <p>
+    <?php $teaser = strip_tags(Str::words($project->body, 50)); ?>
+    {{$teaser}}
+    </p>
     <div><a href="{{$project->slug}}">read more...</a></div>
   </div>
+
 </div>
