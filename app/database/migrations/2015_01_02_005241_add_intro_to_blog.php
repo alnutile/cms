327c9eaba4e489cde57ceba4a28d2f0ed004ae72
Andrew Cavanagh <cavanaghacea@gmail.com>
2015-01-01 21:00:41 -0500

    images

---
+++ app/database/migrations/2015_01_02_005241_add_intro_to_blog.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddIntroToBlog extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+        Schema::table('posts', function(Blueprint $table)
+        {
+            $table->text('intro')->nullable();
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+        Schema::table('posts', function(Blueprint $table)
+        {
+            $table->dropColumn('intro');
+        });
+	}
+
+}
+
