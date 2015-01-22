c814f59bddb0f99c7870c56d403b9049b1e1df53
Andrew Cavanagh <cavanaghacea@gmail.com>
2015-01-22 18:53:59 -0500

    fixes #25

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -3,7 +3,10 @@
 
 class AdminController extends BaseController {
 
-
+    public function __construct()
+    {
+        parent::__construct();
+    }
     public function dash()
     {
       parent::show();
