62e18eadbc4cdc0a81e4fcbe600e85692d03ff80
Andrew Cavanagh <cavanaghacea@gmail.com>
2015-02-23 19:07:36 -0500

    stop replacing spaces with dashes on tags

--- app/views/shared/tags.blade.php
+++ app/views/shared/tags.blade.php
@@ -1,7 +1,10 @@
 <div class="form-group">
     <label>Tags (<a href="http://www.restorationtrades.com/help.html#tags" target="_blank">Help</a>)</label>
     <div ng-controller = "tagsCtrl">
-        <tags-input ng-model="currentTags" on-tag-added="addTag($tag)" on-tag-removed="removeTag($tag)">
+        <tags-input ng-model="currentTags"
+                    on-tag-added="addTag($tag)"
+                    on-tag-removed="removeTag($tag)"
+                    replace-spaces-with-dashes="false">
                     <auto-complete source="getAllTags($query)"></auto-complete>
         </tags-input>
     </div>
