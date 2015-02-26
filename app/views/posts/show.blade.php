74622bbda69faa1d9262531eb46b37a160bec581
Andrew Cavanagh <cavanaghacea@gmail.com>
2015-02-25 19:25:59 -0500

    fixes #69

--- app/views/posts/show.blade.php
+++ app/views/posts/show.blade.php
@@ -25,6 +25,31 @@
     </div>
     <p> {{ $post->body }} </p>
 
+    @if($project->images)
+    @if($settings->theme != true)
+    <div class="help-block">
+        Click on images below to enlarge.
+    </div>
+    @endif
+    @endif
+    <div class = "row gallery_row">
+
+        @foreach ($project->images as $image)
+        @if($settings->theme == false)
+        <div class = "col-lg-6 gallery_item">
+            <a class="gallery" href="/assets/img/projects/{{$image->file_name}}" alt="{{$image->file_name}}" title="{{$image->image_caption}}"><img class="col-lg-12" src="/assets/img/projects/{{$image->file_name}}" alt="{{$image->file_name}}"></a>
+            @else
+            <div class = "col-lg-12 gallery_item_dark">
+                <img class="col-lg-12" src="/assets/img/projects/gallery/{{$image->file_name}}" alt="{{$image->file_name}}">
+                @endif
+                <br>
+                <span class="caption">{{$image->image_caption}}</span>
+            </div>
+            @endforeach
+        </div>
+    </div>
+
+
 </div>
 
 @stop
