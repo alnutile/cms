f576f4812097caecd8bb4d528ba38fc500784948
Andrew Cavanagh <cavanaghacea@gmail.com>
2015-03-03 20:31:25 -0500

    closes issue #77

--- app/controllers/PostsContrtoller.php
+++ app/controllers/PostsContrtoller.php
@@ -24,7 +24,7 @@ class PostsController extends \BaseController {
         $this->post_uri = 'img/posts';
         $this->save_to = public_path() . "/img/posts";
         $this->tags = $tagsService;
-        $this->beforeFilter("auth", array('only' => ['index', 'create', 'delete', 'edit', 'update', 'store']));
+        $this->beforeFilter("auth", array('only' => ['create', 'delete', 'edit', 'update', 'store']));
     }
 
     public function index()
