fdfeb9308bf934efed1974a5546cfcdb7bcbca93
Andrew Cavanagh <cavanaghacea@gmail.com>
2015-03-09 21:05:38 -0400

    fixes #84

--- app/controllers/ProjectsController.php
+++ app/controllers/ProjectsController.php
@@ -93,10 +93,10 @@ class ProjectsController extends \BaseController {
 //            $this->imagesService->cropAndSaveForPages($all['image'], $this->save_to);
         }
 
-        if(isset($data['image_caption_update'])){
+        if(isset($all['image_caption_update'])){
             $this->updateImagesCaption($data['image_caption_update']);
         }
-        if(isset($data['image_order_update'])){
+        if(isset($all['image_order_update'])){
             $this->updateImagesOrder($data['image_order_update']);
         }
 
