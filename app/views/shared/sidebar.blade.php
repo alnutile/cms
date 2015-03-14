d6029b859a7ac0a37cf55603eeda442abea5a538
Andrew Cavanagh <cavanaghacea@gmail.com>
2015-03-14 10:43:50 -0400

    issue #79

--- app/views/shared/sidebar.blade.php
+++ app/views/shared/sidebar.blade.php
@@ -3,30 +3,40 @@
 
 
 
-<div class="well sidebar-nav">
-  <ul class="nav nav-list">
-    <?php
-    if(Request::server('PATH_INFO') == '/portfolios') {
-      $icon = '<i class="glyphicon glyphicon-th"></i>';
-    } else {
-      $icon = '';
-    }
-    ?>
+<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+    <div class="panel panel-default well sidebar-nav">
 
-    <li class="nav-header">
-      <h4>{{$icon}}&nbsp;Links</h4>
-    </li>
-    @foreach($shared_links as $key => $static_menu_item)
-      <?php
-      if(Request::server('PATH_INFO') ==  $static_menu_item) {
-        $active = 'active';
-      } else {
-        $active = 'not-active';
-      }
-      ?>
-      <li class="{{$active}}"> <a href = {{$static_menu_item}}>{{$key}}</a></li>
-    @endforeach
-  </ul>
+        <?php
+        if(Request::server('PATH_INFO') == '/portfolios') {
+            $icon = '<i class="glyphicon glyphicon-th"></i>';
+        } else {
+            $icon = '';
+        }
+        ?>
+        <div class="panel-heading" role="tab" id="headingOne">
+            <a href="#">
+                <h4 class="panel-title nav-header" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                    <big>{{$icon}}&nbsp;Portfolios</big>
+                </h4>
+            </a>
+        </div>
+        <div id="collapseOne" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
+            <div class="panel-body">
+                <ul class="nav nav-list">
+                    @foreach($shared_links as $key => $static_menu_item)
+                    <?php
+                    if(Request::server('PATH_INFO') ==  $static_menu_item) {
+                        $active = 'active';
+                    } else {
+                        $active = 'not-active';
+                    }
+                    ?>
+                    <li class="{{$active}}"> <a href = {{$static_menu_item}}>{{$key}}</a></li>
+                    @endforeach
+                </ul>
+            </div>
+        </div>
+    </div>
 </div>
 
 @include('shared._social')
