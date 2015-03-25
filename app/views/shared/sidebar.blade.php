052ac5ee7d513a3abfedf0540230e48f8e791798
Andrew Cavanagh <cavanaghacea@gmail.com>
2015-03-25 19:20:01 -0400

    fixes issue #79

--- app/views/shared/sidebar.blade.php
+++ app/views/shared/sidebar.blade.php
@@ -1,4 +1,4 @@
-<?php //$portfolios = Portfolio::published()->orderByOrder()->get(); ?>
+<?php //$portfolios = Portfolio::published()->orderByOrder()->get(); dd($shared_links)?>
 
 
 
@@ -13,29 +13,71 @@
             $icon = '';
         }
         ?>
+
+
+        <?php
+        if(Request::server('PATH_INFO') ==  '/') {
+            $active = 'active';
+        } else {
+            $active = 'not-active';
+        }
+        ?>
+        <div class="panel-heading" role="tab">
+                <h4 class="panel-title nav-header" >
+                    <big> <a href = "/">Home</a></big>
+                </h4>
+        </div>
+
         <div class="panel-heading" role="tab" id="headingOne">
             <a href="#">
                 <h4 class="panel-title nav-header" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                    <big>{{$icon}}&nbsp;Portfolios</big>
+                    <big><a href="#">{{$icon}}&nbsp;Portfolios</a></big>
                 </h4>
             </a>
         </div>
         <div id="collapseOne" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
             <div class="panel-body">
                 <ul class="nav nav-list">
-                    @foreach($shared_links as $key => $static_menu_item)
+                    @foreach($portfolio_links as $key => $portfolio)
                     <?php
-                    if(Request::server('PATH_INFO') ==  $static_menu_item) {
+                    if(Request::server('PATH_INFO') ==  $portfolio) {
                         $active = 'active';
                     } else {
                         $active = 'not-active';
                     }
                     ?>
-                    <li class="{{$active}}"> <a href = {{$static_menu_item}}>{{$key}}</a></li>
+                    <li class="{{$active}}"> <a href = {{$portfolio}}>{{$key}}</a></li>
                     @endforeach
                 </ul>
             </div>
         </div>
+
+        <?php
+        if(Request::server('PATH_INFO') ==  '/about') {
+            $active = 'active';
+        } else {
+            $active = 'not-active';
+        }
+        ?>
+        <div class="panel-heading" role="tab">
+            <h4 class="panel-title nav-header" >
+                <big> <a href = "/about">About Page</a></big>
+            </h4>
+        </div>
+
+        <?php
+        if(Request::server('PATH_INFO') ==  '/contact') {
+            $active = 'active';
+        } else {
+            $active = 'not-active';
+        }
+        ?>
+        <div class="panel-heading" role="tab">
+            <h4 class="panel-title nav-header" >
+                <big> <a href = "/contact">Contact Page</a></big>
+            </h4>
+        </div>
+
     </div>
 </div>
 
