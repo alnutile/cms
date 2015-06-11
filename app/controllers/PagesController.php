7f9c14e3e0c791c0633a728570b63810dba81fb4
Andrew Cavanagh <cavanaghacea@gmail.com>
2015-06-11 16:07:30 -0400

    hotfix for updating page title

--- app/controllers/PagesController.php
+++ app/controllers/PagesController.php
@@ -119,26 +119,44 @@ class PagesController extends \BaseController {
          */
         public function update($id)
     {
-        $validator = Validator::make(Input::all(), array('title' => 'required', 'slug' =>'regex:/^\/[A-Za-z0-9_]+$/'));
         $page_update = Input::all();
-        $page = Page::find($id);
 
-        if($validator->passes()) {
-            $page->title = $page_update['title'];
-            $page->body = $page_update['body'];
-            $page->seo = $page_update['seo'];
-            $page->slug = (isset($page_update['slug'])) ?  $page_update['slug'] : $page->slug;
-            if(isset($page_update['images'])) {
-                $this->imagesService->addImages($page->id, $page_update['images'], 'Page');
+        if($this->settings->theme != true) {
+            $validator = Validator::make($page_update, array('title' => 'required', 'slug' => 'regex:/^\/[A-Za-z0-9_]+$/'));
+            $page = Page::find($id);
+            if($validator->passes()) {
+                $page->title = $page_update['title'];
+                $page->body = $page_update['body'];
+                $page->seo = $page_update['seo'];
+                $page->slug = (isset($page_update['slug'])) ?  $page_update['slug'] : $page->slug;
+                if(isset($page_update['images'])) {
+                    $this->imagesService->addImages($page->id, $page_update['images'], 'Page');
+                }
+                $page->save();
+                $banner = $this->bannerSet($page);
+                return Redirect::to("/pages/")->withMessage("Page Updated");
+            } else {
+                return Redirect::to('pages/' . $page->id . '/edit')->withErrors($validator)
+                    ->withMessage("Error ");
             }
-            $page->save();
-            $banner = $this->bannerSet($page);
-            return Redirect::to("/pages/")->withMessage("Page Updated");
-        } else {
-            return Redirect::to('pages/' . $page->id . '/edit')->withErrors($validator)
-                ->withMessage("Error ");
-//            return $this->respond(null, 'pages.edit',  compact('page', 'banner'));
+
+        } else{
+            $validator = Validator::make($page_update, array('slug' => 'regex:/^\/[A-Za-z0-9_]+$/'));
+            $page = Page::find($id);
+            if($validator->passes()) {
+                $page->seo = $page_update['seo'];
+                $page->slug = (isset($page_update['slug'])) ?  $page_update['slug'] : $page->slug;
+                $page->save();
+                $banner = $this->bannerSet($page);
+                return Redirect::to("/pages/")->withMessage("Page Updated");
+            } else {
+                return Redirect::to('pages/' . $page->id . '/edit')->withErrors($validator)
+                    ->withMessage("Error ");
+            }
         }
+
+
+
     }
 
         /**
