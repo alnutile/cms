b25a45a1021e010d058394add0be292e3917055f
John M. Corbett <meetinghouse@users.noreply.github.com>
2015-07-14 12:58:43 -0400

    Update edit.blade.php

--- app/views/posts/edit.blade.php
+++ app/views/posts/edit.blade.php
@@ -11,7 +11,7 @@
 
 
     <div class="form-group">
-        <label>Blog Post Name (<a href="http://www.restorationtrades.com/help.html#portolio_name" target="_blank">Help</a>)</label>
+        <label>Blog Post Name (<a href="http://www.restorationtrades.com/help/blog.html" target="_blank">Help</a>)</label>
         {{ Form::text('title', null, array('class' => 'form-control')) }}
     </div>
     @if($errors->first('title'))
