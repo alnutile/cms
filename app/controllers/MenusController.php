5dfadb06c8f19fbb708d8e252e1cfa8ed4225056
vivek <vivek@itenterprise.co.uk>
2016-11-08 16:07:57 +0530

    Allow to add sub menus for light theme

--- app/controllers/MenusController.php
+++ app/controllers/MenusController.php
@@ -18,10 +18,42 @@ class MenusController extends \BaseController {
   }
 	public function index()
 	{
-      parent::show();
-        $menus = Page::where("slug", "!=", "")->orderBy("menu_sort_order")->get();
+		parent::show();
+        $menus = Page::where("slug", "!=", "")->orderBy("menu_sort_order")->get()->toArray();
         $banner = $this->banner;
-        return $this->respond($menus, 'menus.index',  compact('menus', 'banner'));
+		$sub_menus = array_filter($menus, function ($menu) {
+			return $menu['menu_parent'] != 0;
+		});
+		$sub_menus = array_values($sub_menus);
+		$removed_index = array();
+		for( $i = 0; $i<count($menus); $i++)
+		{
+			foreach( $sub_menus as $sub_menu)
+			{
+				if($menus[$i]['id'] == $sub_menu['id'])
+				{
+					$removed_index[] = $i; 
+				}
+			}
+		}
+		for($i = 0; $i<count($removed_index);$i++)
+		{
+			unset($menus[$removed_index[$i]]);
+		}
+		$menus = array_values($menus);
+		for( $i = 0; $i<count($menus); $i++)
+		{
+			foreach( $sub_menus as $sub_menu)
+			{
+				if($menus[$i]['id'] == $sub_menu['menu_parent'])
+				{
+					$menus[$i]['sub_menus'][] = $sub_menu;
+				}
+			}
+		}
+		unset($removed_index);
+		unset($sub_menus);
+		return $this->respond($menus, 'menus.index',  compact('menus', 'banner'));
     }
 
     public function store()
--- app/views/menus/index.blade.php
+++ app/views/menus/index.blade.php
@@ -8,16 +8,28 @@
     <h3>Drag and drop to manage your <strong>Top Right Menu</strong></h3>
 
             <ol class="top-menu">
-            @foreach($menus as $menu)
-
-                <li
-                    data-name="{{{$menu->title}}}"
-                    data-page-id="{{$menu->id}}"
-                    data-page-menu-parent="{{$menu->menu_parent}}"
-                    data-menu-location="{{$menu->menu_name}}">
-                    <i class="glyphicon glyphicon-move">
-
-                    </i>&nbsp;{{{$menu->title}}} {{$menu->slug}}
+			@foreach($menus as $menu)
+				<li
+                    data-name="{{$menu['title']}}"
+                    data-page-id="{{$menu['id']}}"
+                    data-page-menu-parent="{{$menu['menu_parent']}}"
+                    data-menu-location="{{$menu['menu_name']}}">
+                    <i class="glyphicon glyphicon-move"></i>&nbsp;{{$menu['title']}}
+					@if(array_key_exists('sub_menus', $menu))
+						<ol>
+							@foreach($menu['sub_menus'] as $sub_menu)
+							 <li
+								 data-name="{{$sub_menu['title']}}"
+								data-page-id="{{$sub_menu['id']}}"
+								data-page-menu-parent="{{$menu['id']}}"
+								data-menu-location="{{$sub_menu['menu_name']}}">
+								<i class="glyphicon glyphicon-move"></i>&nbsp;{{$sub_menu['title']}}
+							</li>
+							@endforeach
+						</ol>
+					@else
+						<ol></ol>
+					@endif
                 </li>
             @endforeach
             </ol>
