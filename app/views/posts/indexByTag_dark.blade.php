9ea6fddce0f42c39a94acfbd1ba788f61697d3dc
vivek <vivek@itenterprise.co.uk>
2016-11-11 18:48:26 +0530

    Provide n-level navigations and revised changes for read more links

--- app/views/posts/indexByTag_dark.blade.php
+++ app/views/posts/indexByTag_dark.blade.php
@@ -17,22 +17,20 @@
     <div class="portfolio-wrap">
         @foreach($posts as $p)
         <div class="row blog_row">
-            <a href="{{$p->slug}}">
-                <div class="post_intro col-md-9">
-                    <h2 class="media-heading">{{{$p->title}}}</h2>
-                    {{ $p->intro }}
-                </div>
+           <div class="blog_row">
+           <div class="post_intro col-md-9">
+				<h2 class="media-heading"><a href="{{$p->slug}}">{{{$p->title}}}</a></h2>
+				{{ $p->intro }} <span><a href="{{$p->slug}}">Read more...</a></span>
+			</div>
 
-                <div class="post_img col-md-3">
-                    @if ($p->image)
-                    <img  src="/img/posts/thumb/{{$p->image}}" alt="{{$p->title}}" class="img-responsive">
-                    @else
-                    {{--<img  src="/img/default/photo_default_0.png" alt="{{$p->title}}" class="img-responsive">--}}
-                    @endif
-                </div>
-            </a>
+			<div class="post_img col-md-3">
+				@if ($p->image)
+				<img  src="/img/posts/thumb/{{$p->image}}" alt="{{$p->title}}" class="img-responsive">
+				@else
+				{{--<img  src="/img/default/photo_default_0.png" alt="{{$p->title}}" class="img-responsive">--}}
+				@endif
+			</div>
         </div>
-		<div class="row"><a href="{{$p->slug}}">Read more...</a></div>
         @endforeach
     </div>
 </div>
