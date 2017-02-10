4d1b8c9bb90cd1856d22d66c50c480fb1c63c369
vivek <vivek@hastishah.com>
2017-02-10 11:00:13 +0530

    Change line of message

--- app/views/errors/404.blade.php
+++ app/views/errors/404.blade.php
@@ -2,6 +2,6 @@
 @section('content')
 	<div class="col-md-12 aligncenter">
 		<h1><span class="base-clr-txt">404</span> - page not found!</h1>
-		<p>Oops..., the page you are trying to reach no longer exists or the URL you typed is incorrect. Please select a page from the menu. Or something similar :(</p>
+		<p>Oops..., the page you are trying to reach no longer exists or the URL you typed is incorrect. Please select a page from the menu:(.</p>
 	</div>
 @stop
