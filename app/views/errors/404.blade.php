2b5ea7bf968ee5a61ed8627d8412ce5b4963b358
Vivek Chaudhari <vivek@hastishah.com>
2017-02-21 20:26:37 +0530

    Vivek work (#291)
    * Remove extra rows
    
    * Add read more class on index files.
    
    Add read more class on index files.
    
    * Fix blog tag layout
    
    Add missing div to fix blog tag layout.
    
    * Remove extra blog_row class
    
    * Add n-level navs to light theme left sidebar
    
    * Changes to index blades
    
    Changes to index blades light and dark themes
    
    * Change menu tree view query.
    
    * resolved undefined variable issue
    
    * menu item displaying issue
    
    * Design and setup 404 page for sites.
    
    * Change line of message
    
    * Add class to 404 page
    
    * Revert "Add class to 404 page"
    
    This reverts commit 885d8df3b52d691b59b27a2e53d37a846efcc148.
    
    * Add class to 404 page.

--- app/views/errors/404.blade.php
+++ app/views/errors/404.blade.php
@@ -1,7 +1,7 @@
 @extends('errors.main')
 @section('content')
-	<div class="col-md-12 aligncenter">
+	<div class="col-md-12 aligncenter four-o-four">
 		<h1><span class="base-clr-txt">404</span> - page not found!</h1>
-		<p>Oops..., the page you are trying to reach no longer exists or the URL you typed is incorrect. Please select a page from the menu:(.</p>
+		<p>Oops..., the page you are trying to reach no longer exists or the URL you typed is incorrect. Please select a page from the menu.</p>
 	</div>
 @stop
