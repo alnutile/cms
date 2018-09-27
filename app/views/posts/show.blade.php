304990282db40b2890c300fa1c2d1b5fc87ce9f5
Vivek Chaudhari <vivekchaudhari087@gmail.com>
2018-09-27 16:09:11 +0530

    change the code

--- app/views/posts/show.blade.php
+++ app/views/posts/show.blade.php
@@ -29,7 +29,9 @@
 	   <div class="row">        
 			<div class = "col-lg-12" id="main_image">
 				@if(file_exists(public_path().'/img/posts/full/'.$post->image))
-					<img  src="/img/posts/full/{{$post->image}}" alt="{{$post->title}}" class="img-responsive">			
+					<img  src="/img/posts/full/{{$post->image}}" alt="{{$post->title}}" class="img-responsive">	
+				@else
+					<img  src="/img/posts/{{$post->image}}" alt="{{$post->title}}" class="img-responsive">
 				@endif			
 			</div>        
 		</div>
