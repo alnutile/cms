78d3e34d6cf5689fa4ebefe8ea1eb923e9e709f4
Vivek Chaudhari <vivek@hastishah.com>
2018-10-16 23:31:28 +0530

    change statement

--- app/database/migrations/2018_10_16_175049_alter_table_projects_rename_architect_cols.php
+++ app/database/migrations/2018_10_16_175049_alter_table_projects_rename_architect_cols.php
@@ -13,7 +13,7 @@ class AlterTableProjectsRenameArchitectCols extends Migration {
 	public function up()
 	{
 		//
-		DB::statement("ALTER TABLE `projects` RENAME COLUMN `architect` TO `participant3`;");
+		DB::statement("ALTER TABLE  `projects` CHANGE  `architect`  `participant3` VARCHAR( 255 ) DEFAULT NULL ;");
 	}
 
 	/**
