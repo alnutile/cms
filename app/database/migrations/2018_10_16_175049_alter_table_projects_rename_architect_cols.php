a62cdb5bdbf29035d574dd50b195c58ef62cac75
Vivek Chaudhari <vivek@hastishah.com>
2018-10-16 23:53:50 +0530

    rename class

--- app/database/migrations/2018_10_16_175049_alter_table_projects_rename_architect_cols.php
+++ app/database/migrations/2018_10_16_175049_alter_table_projects_rename_architect_cols.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AlterTableProjectsRenameArchitectCols extends Migration {
+class RenameAlterProjectsFieldArchitectToParticipant3 extends Migration {
 
 	/**
 	 * Run the migrations.
