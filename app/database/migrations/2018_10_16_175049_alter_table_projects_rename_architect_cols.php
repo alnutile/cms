837f4c0fdf0a57242c42437d6b22b34e5b97b648
Vivek Chaudhari <vivek@hastishah.com>
2018-10-16 23:55:18 +0530

    undo class name

--- app/database/migrations/2018_10_16_175049_alter_table_projects_rename_architect_cols.php
+++ app/database/migrations/2018_10_16_175049_alter_table_projects_rename_architect_cols.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class RenameAlterProjectsFieldArchitectToParticipant3 extends Migration {
+class AlterTableProjectsRenameArchitectCols extends Migration {
 
 	/**
 	 * Run the migrations.
