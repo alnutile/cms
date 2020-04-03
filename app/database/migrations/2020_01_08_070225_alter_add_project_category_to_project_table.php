f27307f7e305cc4a8e97e9864ca869be60b8fb14
Vivek Chaudhari <vivekchaudhari087@gmail.com>
2020-04-03 14:31:08 +0530

    change conditions

--- app/database/migrations/2020_01_08_070225_alter_add_project_category_to_project_table.php
+++ app/database/migrations/2020_01_08_070225_alter_add_project_category_to_project_table.php
@@ -14,7 +14,7 @@ class AlterAddProjectCategoryToProjectTable extends Migration {
 	{
 		Schema::table('projects', function(Blueprint $table)
 		{
-			if(Schema::hasColumn('projects', 'project_category'))
+			if(!Schema::hasColumn('projects', 'project_category'))
 			{
 				$table->text('project_category')->nullable();
 			}
